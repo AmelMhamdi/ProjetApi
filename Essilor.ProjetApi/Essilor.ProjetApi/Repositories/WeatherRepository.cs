89d7b496d62d4e7650fe1d5f001b45186fa2f347
AmelMhamdi <amel.mhamdi2@gmail.com>
2019-05-11 15:11:05 +0200

    initialize the structure of project

---
+++ Essilor.ProjetApi/Essilor.ProjetApi/Repositories/WeatherRepository.cs
@@ -0,0 +1,12 @@
+﻿using Essilor.ProjetApi.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Essilor.ProjetApi.Repositories
+{
+    public class WeatherRepository: IWeatherRepository
+    {
+    }
+}
